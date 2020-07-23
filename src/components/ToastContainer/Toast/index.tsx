5b7edd88349969c3c6180c1ffb9f65543b77f4b2
Jean Santos <jeansjcsp@gmail.com>
2020-07-23 14:17:46 -0300

    Adding and removing toasts.

---
+++ src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
+} from 'react-icons/fi';
+
+import { ToastMessage, useToast } from '../../../hooks/Toast';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  toastData: ToastMessage;
+}
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
+const Toast: React.FC<ToastProps> = ({ toastData }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(toastData.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, toastData.id]);
+
+  return (
+    <Container type={toastData.type} hasDescription={!!toastData.description}>
+      {(toastData.type && icons[toastData.type]) || icons.info}
+      <div>
+        <strong>{toastData.title}</strong>
+        {toastData.description && <p>{toastData.description}</p>}
+      </div>
+
+      <button onClick={() => removeToast(toastData.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
